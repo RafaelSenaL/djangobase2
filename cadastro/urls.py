18fa911a98f20d93b0a1437d4e1c0a667367158b
RafaelSenaL <rafael.sena@aluno.senac.rio>
2026-04-14 19:49:16 -0300

    ajuste

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -1,8 +1,22 @@
+# cadastro\urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),             
-    path('contato/', views.contato, name='contato'), 
-    path('adicionar/', views.adicionar, name='adicionar')
+    # Rota da página inicial
+    path('', views.index, name='index'),
+
+    # Rota da página 'Contatos'
+    path('contato/', views.contato, name='contato'),
+
+    # Rota para cadastrar pessoa
+    path('new/', views.adicionar, name='adicionar'),
+
+    # Rota para os detalhes de uma pessoa
+    path('pessoa/<int:id>/', views.detalhe, name='detalhe'),
+
+    path('pessoa/<int:id>/editar/', views.editar, name='editar'),
+
+    path('pessoa/<int:id>/deletar/', views.deletar, name='deletar'),
 ]
--- cadastro/views.py
+++ cadastro/views.py
@@ -1,15 +1,11 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Pessoa
 from .forms import PessoaForm
 
 def index(request):
     pessoas = Pessoa.objects.all()
-    total = Pessoa.objects.count()
-    #dicionario que passa dados para o template.
     contexto = {
         'pessoas': pessoas,
-        'total': total,
-       
     }
     return render(request, 'cadastro/index.html', contexto)
 
@@ -25,3 +21,25 @@ def adicionar(request):
     else:
         form = PessoaForm()
     return render(request, 'cadastro/adicionar.html', {'form': form})
+
+def detalhe(request, id):
+    pessoa = get_object_or_404(Pessoa, id=id)
+    return render(request, 'cadastro/detalhe.html', {'pessoa': pessoa})
+
+def editar(request, id):
+    pessoa = get_object_or_404(Pessoa, id=id)
+    if request.method == 'POST':
+        form = PessoaForm(request.POST, instance=pessoa)
+        if form.is_valid():
+            form.save()
+            return redirect('detalhe', id=id)
+    else:
+        form = PessoaForm(instance=pessoa)
+    return render(request, 'cadastro/editar.html', {'form': form, 'pessoa': pessoa})
+
+def deletar(request, id):
+    pessoa = get_object_or_404(Pessoa, id=id)
+    if request.method == 'POST':
+        pessoa.delete()
+        return redirect('index')
+    return render(request, 'cadastro/deletar.html', {'pessoa': pessoa})
